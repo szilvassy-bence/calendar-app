a51ae2a01cd32bc132e7490e65454f19f05ff23b
Szilvássy Bence <benceszilvassy91@gmail.com>
2024-07-10 15:30:25 +0200

    updated api.php and web.php, we use api.php

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\BookingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::prefix('/bookings')->group(function (){
+//    Route::get('/', [BookingController::class, 'index'])
+//        ->name('bookings.index');
+//});
+
+Route::get('/bookings', [BookingController::class, 'index'])
+    ->name('bookings.index');
+
+Route::post('/bookings', [BookingController::class, 'store'])
+    ->name('bookings.store');
+
