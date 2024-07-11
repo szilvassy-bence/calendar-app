3c0b091943d5d3fb8b9099f4a81445825ed8fbe4
Szilvássy Bence <benceszilvassy91@gmail.com>
2024-07-11 12:19:27 +0200

    added error handling for basic index endpoint

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\BookingRequest;
 use App\Models\Booking;
 use App\Repositories\BookingRepository;
 use App\Services\BookingService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Config;
@@ -16,13 +17,17 @@ class BookingController extends Controller
 
     public function __construct(protected BookingRepository $bookingRepository)
     {}
-    public function index()
+    public function index(): JsonResponse
     {
-        $bookings = Booking::all();
-        return response()->json($bookings);
+        try {
+            $bookings = Booking::all();
+            return response()->json($bookings);
+        } catch (HttpException $e) {
+            return response()->json(['message' => $e->getMessage()], $e->getStatusCode());
+        }
     }
 
-    public function store(BookingRequest $request)
+    public function store(BookingRequest $request): JsonResponse
     {
         try {
             $id = $this->bookingRepository->store($request);
@@ -32,7 +37,7 @@ class BookingController extends Controller
         }
     }
 
-    public function getOpeningHours()
+    public function getOpeningHours(): JsonResponse
     {
         return response()->json([
             'opening_hour' => Config::get('office.opening_hour'),
