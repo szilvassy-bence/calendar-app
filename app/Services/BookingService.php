24e1ef11511b8b7232e41188c8c740abdaa103ae
Szilvássy Bence <benceszilvassy91@gmail.com>
2024-07-11 15:34:35 +0200

    updated BookingService.php uses Constants for opening and closing hours

--- app/Services/BookingService.php
+++ app/Services/BookingService.php
@@ -4,56 +4,52 @@ namespace App\Services;
 
 use App\Enums\BookingRepetition;
 use App\Enums\Day;
-use App\Http\Requests\BookingRequest;
 use App\Models\Booking;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Config;
-use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class BookingService
 {
-    // if they have collision returns true
-    // if they do not have collision returns false
-    public function isOccupied(Booking $dbBooking, Booking $request): bool {
+
+    const OPENING_HOUR = 8;
+    CONST CLOSING_HOUR = 20;
+
+    public function isOccupied(Booking $dbBooking, Booking $request): bool
+    {
         $dbBookingEndDate = $dbBooking->end_date ?? Carbon::create(9000);
         $requestEndDate = $request->end_date ?? Carbon::create(9000);
 
-        if ($dbBooking->repetition === BookingRepetition::NO && $request->repetition === BookingRepetition::NO)
-        {
+        if ($dbBooking->repetition === BookingRepetition::NO && $request->repetition === BookingRepetition::NO) {
             return $dbBooking->start_date->isSameDay($request->start_date);
-        }
-        else if ($dbBooking->repetition === BookingRepetition::NO && $request->repetition !== BookingRepetition::NO)
-        {
+        } elseif ($dbBooking->repetition === BookingRepetition::NO && $request->repetition !== BookingRepetition::NO) {
             return $dbBooking->start_date->greaterThanOrEqualTo($request->start_date)
                 && $dbBooking->start_date->lessThanOrEqualTo($requestEndDate);
-        }
-        else if ($dbBooking->repetition !== BookingRepetition::NO && $request->repetition === BookingRepetition::NO)
-        {
+        } elseif ($dbBooking->repetition !== BookingRepetition::NO && $request->repetition === BookingRepetition::NO) {
             return $dbBooking->start_date->lessThanOrEqualTo($request->start_date)
                 && $dbBookingEndDate->greaterThanOrEqualTo($request->start_date);
-        }
-        else if ($dbBooking->repetition === $request->repetition
+        } elseif ($dbBooking->repetition === $request->repetition
                 || $dbBooking->repetition === BookingRepetition::WEEKS
                 || $request->repetition === BookingRepetition::WEEKS
-        )
-        {
+        ) {
             return $dbBooking->start_date->lessThanOrEqualTo($requestEndDate)
                     && $dbBookingEndDate->greaterThanOrEqualTo($request->start_date);
         }
         return false;
     }
 
-    public function isInOpeningHours(Booking $booking) {
-        $openingHour = Config::get('office.opening_hour');
-        $closingHour = Config::get('office.closing_hour');
+    public function isInOpeningHours(Booking $booking)
+    {
+
+//        $openingHour = Config::get('office.opening_hour');
+//        $closingHour = Config::get('office.closing_hour');
 
         if ($booking->day === Day::SATURDAY || $booking->day === Day::SUNDAY) {
             return false;
         }
-        if ($booking->start_time < Carbon::createFromTime($openingHour)) {
+        if ($booking->start_time < Carbon::createFromTime(self::OPENING_HOUR)) {
             return false;
         }
-        if ($booking->end_time > Carbon::createFromTime($closingHour)) {
+        if ($booking->end_time > Carbon::createFromTime(self::CLOSING_HOUR)) {
             return false;
         }
         return true;
